e8a08b5a39bc1bcb12942bb284700050073ae5cf
Juan Pablo Perez <Juan.Pablo.Perez@retailinmotion.com>
2018-12-03 18:35:09 +0000

    A Multi-Container App with Kubernetes

---
+++ elastic-beanstalk/Worker/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ServiceStack.Redis;
+
+namespace Worker
+{
+    class Program
+    {
+        static void Main()
+        {
+            var redisHost = Environment.GetEnvironmentVariable("REDIS_HOST");
+            var redisPort = Environment.GetEnvironmentVariable("REDIS_PORT");
+            var redis = new RedisManagerPool($"{redisHost}:{redisPort}");
+            using (var redisConsumer  = redis.GetClient())
+            using (var subscription = redisConsumer.CreateSubscription())
+            {
+                subscription.OnMessage = (channel, msg) =>
+                {
+                    const string setKey = "values";
+                    var redisKey = setKey + msg;                    
+                    using (var redisClient = redis.GetClient())
+                    {
+                        redisClient.SetValue(redisKey, Fib(Convert.ToInt32(msg)).ToString());
+                        redisClient.AddItemToSet(setKey, redisKey);
+                    }                    
+                };    
+                subscription.SubscribeToChannels("message");          
+            }
+        }
+
+        private static int Fib(int index) 
+        {
+            if (index < 2) return 1;
+            return Fib(index - 1) + Fib(index - 2);         
+        }
+    }
+
+}
